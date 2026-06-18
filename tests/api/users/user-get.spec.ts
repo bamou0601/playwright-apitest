94bc6a033bb93403dbc8cd794fbeb234eae36af9
bamou0601 <mo.ba@ves.co.jp>
2026-06-18 17:42:07 +0900

    optimiaze code

--- tests/api/users/user-get.spec.ts
+++ tests/api/users/user-get.spec.ts
@@ -1,5 +1,15 @@
 import { test, expect } from '@playwright/test';
 import { UserApi } from '../../../api/UserApi';
+import getData from '../../../test-data/users/get-data.json';
+
+/**
+ * ユーザー情報取得APIテスト
+ *
+ * 指定した ID のユーザー情報を正常に取得できることを検証する。
+ *
+ * 作成者: 馬 猛
+ * 作成日: 2026/06/18
+ */
 
 test('ユーザー情報取得', async ({ request }) => {
 
@@ -7,17 +17,17 @@ test('ユーザー情報取得', async ({ request }) => {
     const userApi = new UserApi(request);
 
     // ユーザー情報取得API実行
-    const response = await userApi.getUser(1);
+    const response = await userApi.getUser(getData.id);
 
     // レスポンスの検証
     expect(response.ok()).toBeTruthy();
-    expect(response.status()).toBe(200);
+    expect(response.status()).toBe(getData.expected.status);
 
     const body = await response.json();
 
     // expect(body).toMatchSnapshot(
     //     'user-1.json'
     // );
-    expect(body.id).toBe(1);
+    expect(body.id).toBe(getData.id);
 
 });
